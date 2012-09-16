25f4e063235aa3cada94b6ad137907c068721ccf
Devin Sevilla <dasevilla@gmail.com>
2012-09-16 16:45:47 -0700

    Add description for 'call' method

--- rdiocli/call.py
+++ rdiocli/call.py
@@ -4,6 +4,10 @@ from command import RdioCommand
 
 
 class RdioCall(RdioCommand):
+
+    def get_description(self):
+        return "Make an OAuth 2.0 API call"
+
     def get_parser(self, prog_name):
         parser = super(RdioCall, self).get_parser(prog_name)
         parser.add_argument('method', help='api method to call')
