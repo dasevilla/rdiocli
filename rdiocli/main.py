5cb99ad4cf69a3d79e1262b1afa14729ad24a235
Devin Sevilla <dasevilla@gmail.com>
2012-09-16 15:32:06 -0700

    Remove unused methods

--- rdiocli/main.py
+++ rdiocli/main.py
@@ -27,23 +27,6 @@ class RdioApp(App):
         else:
             self.rdio_session = requests.session(auth=BearerAuth(auth_code))
 
-    def prepare_to_run_command(self, cmd):
-        self.log.debug('prepare_to_run_command %s', cmd.__class__.__name__)
-
-    def clean_up(self, cmd, result, err):
-        self.log.debug('clean_up %s', cmd.__class__.__name__)
-        if err:
-            self.log.debug('got an error: %s', err)
-
-    def call_rdio(self, method, params=dict()):
-        api_url = 'https://www.rdio.com/api/1/'
-        params['method'] = method
-        result = self.rdio_session.post(api_url, data=params)
-
-        if result.json['status'] == 'error':
-            raise RuntimeError('Rdio API error: %s' % result.json['message'])
-        return result.json
-
     def api_call(self, parsed_args):
         payload = {
             'method': parsed_args.method
